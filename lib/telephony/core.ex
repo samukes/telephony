4b8a56be4da48d8292f82d862ededc3899d8e3c0
Samuel Fontes <darkdarumia@gmail.com>
2023-07-14 18:44:04 -0300

    feat: 1.2.14-1

--- lib/telephony/core.ex
+++ lib/telephony/core.ex
@@ -15,4 +15,60 @@ defmodule Telephony.Core do
 
   def create_subscribers(_, _),
     do: {:error, "Only 'prepaid' or 'postpaid' are accepted"}
+
+  def search_subscriber(subscribers, phone_number) do
+    Enum.find(subscribers, &(&1.phone_number == phone_number))
+  end
+
+  def make_recharge(subscribers, phone_number, value, date) do
+    subscribers
+    |> search_subscriber(phone_number)
+    |> then(fn subscriber ->
+      if is_nil(subscriber) do
+        subscribers
+      else
+        subscribers = List.delete(subscribers, subscriber)
+        result = Subscriber.make_recharge(subscriber, value, date)
+        update_subscriber(subscribers, result)
+      end
+    end)
+  end
+
+  defp update_subscriber(subscribers, {:error, _} = err) do
+    {subscribers, err}
+  end
+
+  defp update_subscriber(subscribers, subscriber) do
+    {subscribers ++ [subscriber], subscriber}
+  end
+
+  def make_call(subscribers, phone_number, time_spent, date) do
+    subscribers
+    |> search_subscriber(phone_number)
+    |> then(fn subscriber ->
+      if is_nil(subscriber) do
+        subscribers
+      else
+        subscribers = List.delete(subscribers, subscriber)
+        result = Subscriber.make_call(subscriber, time_spent, date)
+        update_subscriber(subscribers, result)
+      end
+    end)
+  end
+
+  def print_invoice(subscribers, phone_number, year, month) do
+    subscribers
+    |> search_subscriber(phone_number)
+    |> then(fn subscriber ->
+      if is_nil(subscriber) do
+        subscribers
+      else
+        Subscriber.print_invoice(subscriber, year, month)
+      end
+    end)
+  end
+
+  def print_invoices(subscribers, year, month) do
+    Enum.map(subscribers, &Subscriber.print_invoice(&1, year, month))
+  end
 end
--- test/telephony/core_test.exs
+++ test/telephony/core_test.exs
@@ -1,7 +1,7 @@
 defmodule Telephony.CoreTest do
   use ExUnit.Case
   alias Telephony.Core
-  alias Telephony.Core.{Prepaid, Subscriber}
+  alias Telephony.Core.{Postpaid, Prepaid, Recharge, Subscriber}
 
   setup do
     subscribers = [
@@ -77,5 +77,172 @@ defmodule Telephony.CoreTest do
     result = Core.create_subscribers([], payload)
 
     assert {:error, "Only 'prepaid' or 'postpaid' are accepted"} == result
+  end
+
+  test "search a subscriber", %{subscribers: subscribers} do
+    expect = %Subscriber{
+      full_name: "Samuel",
+      phone_number: 123,
+      type: :prepaid
+    }
+
+    result = Core.search_subscriber(subscribers, 123)
+
+    assert expect == result
+  end
+
+  test "return nil when subscriber does not exist", %{subscribers: subscribers} do
+    result = Core.search_subscriber(subscribers, 000)
+
+    assert nil == result
+  end
+
+  test "make a recharge prepaid" do
+    subscribers = [
+      %Subscriber{
+        full_name: "Samuel",
+        phone_number: 123,
+        type: %Prepaid{credits: 0, recharges: []}
+      }
+    ]
+
+    date = Date.utc_today()
+
+    result = Core.make_recharge(subscribers, 123, 2, date)
+
+    assert {[
+              %Subscriber{
+                full_name: "Samuel",
+                phone_number: 123,
+                type: %Prepaid{
+                  credits: 2,
+                  recharges: [%Recharge{value: 2, date: date}]
+                },
+                calls: []
+              }
+            ],
+            %Subscriber{
+              full_name: "Samuel",
+              phone_number: 123,
+              type: %Prepaid{
+                credits: 2,
+                recharges: [%Recharge{value: 2, date: date}]
+              },
+              calls: []
+            }} == result
+  end
+
+  test "make a recharge postpaid" do
+    subscribers = [
+      %Subscriber{
+        full_name: "Samuel",
+        phone_number: 123,
+        type: %Postpaid{spent: 0}
+      }
+    ]
+
+    date = Date.utc_today()
+
+    result = Core.make_recharge(subscribers, 123, 2, date)
+
+    assert {[], {:error, "Postpaid can't do a recharge"}} == result
+  end
+
+  test "make a call prepaid" do
+    subscribers = [
+      %Subscriber{
+        full_name: "Samuel",
+        phone_number: 123,
+        type: %Prepaid{credits: 0, recharges: []}
+      }
+    ]
+
+    expect = {[], {:error, "Subscriber does not have credits"}}
+
+    date = Date.utc_today()
+    result = Core.make_call(subscribers, 123, 1, date)
+
+    assert expect == result
+  end
+
+  test "print invoice" do
+    subscribers = [
+      %Subscriber{
+        full_name: "Samuel",
+        phone_number: 123,
+        type: %Prepaid{credits: 0, recharges: []}
+      }
+    ]
+
+    expect = %{
+      invoice: %{calls: [], credits: 0, recharges: []},
+      subscirber: %Subscriber{
+        full_name: "Samuel",
+        phone_number: 123,
+        type: %Prepaid{credits: 0, recharges: []},
+        calls: []
+      }
+    }
+
+    date = Date.utc_today()
+    result = Core.print_invoice(subscribers, 123, date.year, date.month)
+
+    assert expect == result
+  end
+
+  test "print all invoices" do
+    subscribers = [
+      %Subscriber{
+        full_name: "Samuel",
+        phone_number: 123,
+        type: %Prepaid{credits: 5, recharges: []}
+      },
+      %Subscriber{
+        full_name: "Carlos",
+        phone_number: 456,
+        type: %Prepaid{credits: 2.6, recharges: []}
+      },
+      %Subscriber{
+        full_name: "Arlete",
+        phone_number: 789,
+        type: %Postpaid{spent: 100}
+      }
+    ]
+
+    date = Date.utc_today()
+
+    expect = [
+      %{
+        invoice: %{calls: [], credits: 5, recharges: []},
+        subscirber: %Subscriber{
+          full_name: "Samuel",
+          phone_number: 123,
+          type: %Prepaid{credits: 5, recharges: []},
+          calls: []
+        }
+      },
+      %{
+        invoice: %{calls: [], credits: 2.6, recharges: []},
+        subscirber: %Subscriber{
+          full_name: "Carlos",
+          phone_number: 456,
+          type: %Prepaid{credits: 2.6, recharges: []},
+          calls: []
+        }
+      },
+      %{
+        invoice: %{calls: [], value_spent: 0},
+        subscirber: %Subscriber{
+          full_name: "Arlete",
+          phone_number: 789,
+          type: %Postpaid{spent: 100},
+          calls: []
+        }
+      }
+    ]
+
+    result = Core.print_invoices(subscribers, date.year, date.month)
+
+    assert expect == result
   end
 end
